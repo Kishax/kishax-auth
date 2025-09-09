8c76bb21757e34a21678396381ea3569d82558e4
Takaya Maekawa <takaya@maekawa.dev>
2025-09-10 00:20:31 +0900

    SCRUM-124: メールアドレスとパスワードでのログインにOTP送信を経由する

--- scripts/start-with-template.sh
+++ scripts/start-with-template.sh
@@ -13,9 +13,18 @@ content="${content//\$\{GITHUB_CLIENT_SECRET\}/${GITHUB_CLIENT_SECRET}}"
 content="${content//\$\{IAM_IDENTITY_CENTER_ACS_URL\}/${IAM_IDENTITY_CENTER_ACS_URL}}"
 content="${content//\$\{IAM_IDENTITY_CENTER_ISSUER_URL\}/${IAM_IDENTITY_CENTER_ISSUER_URL}}"
 
+# Replace SMTP environment variables
+content="${content//\$\{SMTP_HOST\}/${SMTP_HOST}}"
+content="${content//\$\{SMTP_PORT\}/${SMTP_PORT}}"
+content="${content//\$\{SMTP_FROM\}/${SMTP_FROM}}"
+content="${content//\$\{SMTP_FROM_DISPLAY_NAME\}/${SMTP_FROM_DISPLAY_NAME}}"
+content="${content//\$\{SMTP_USER\}/${SMTP_USER}}"
+content="${content//\$\{SMTP_PASSWORD\}/${SMTP_PASSWORD}}"
+content="${content//\$\{SMTP_SSL\}/${SMTP_SSL}}"
+content="${content//\$\{SMTP_STARTTLS\}/${SMTP_STARTTLS}}"
+
 # Write processed content to output file
 echo "$content" >"$output_file"
 
 # Start Keycloak with realm import (overwrite existing)
-exec /opt/keycloak/bin/kc.sh start --import-realm --spi-import-strategy=overwrite-existing
-
+exec /opt/keycloak/bin/kc.sh start-dev --import-realm --spi-import-strategy=overwrite-existing
